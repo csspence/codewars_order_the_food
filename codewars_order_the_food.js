99e04729d6579429b46e369af69b7f4dc2879d2e
Crew Spence <csspence123@gmail.com>
2021-05-21 08:08:42 -0500

    Update codewars_order_the_food.js

--- codewars_order_the_food.js
+++ codewars_order_the_food.js
@@ -1,8 +1,6 @@
 /*
 You will be given an array of objects representing data about developers who have signed up to attend the next coding meetup that you are organising.
-
 Your task is to return an object which includes the count of food options selected by the developers on the meetup sign-up form..
-
 For example, given the following input array:
 
 var list1 = [
